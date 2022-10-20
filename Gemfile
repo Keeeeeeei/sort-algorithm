9429c8a7193eb094bf2ab2959988f57cdad2962e
Keeeeeeei <1372012@g.iuhw.ac.jp>
2022-10-20 22:49:26 +0900

    rubocop導入とコード修正

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
-
-
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
 git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem 'rubocop', require:false
+gem 'rubocop', require: false
 gem 'rubocop-performance', require: false
 gem 'rubocop-rails', require: false
 gem 'rubocop-rspec'
---
+++ algorithm/sort_algorithm.rb
@@ -0,0 +1,12 @@
+default_list = [10, 8, 3, 5, 2, 4, 11, 18, 20, 33]
+(0..default_list.length - 1).each do |i|
+  (0..(default_list.length - i - 2)).each do |j|
+    next unless default_list[j] > default_list[j + 1]
+
+    tmp = default_list[j]
+    default_list[j] = default_list[j + 1]
+    default_list[j + 1] = tmp
+  end
+end
+result = default_list.to_s
+puts result
